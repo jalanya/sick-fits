0dfb05aea3d3582515df60e3702db71a10094a47
Jose Alanya <jose.alanya@gmail.com>
2021-05-19 17:48:04 -0500

    FEAT: User Registration + Authentication: Creating our Sign Up Flow

--- frontend/pages/signin.js
+++ frontend/pages/signin.js
@@ -1,9 +1,18 @@
+import styled from 'styled-components';
 import SignIn from '../components/SignIn';
+import SignUp from '../components/SignUp';
+
+const GridSytles = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 2rem;
+`;
 
 export default function SingInPage() {
   return (
-    <div>
+    <GridSytles>
       <SignIn />
-    </div>
+      <SignUp />
+    </GridSytles>
   );
 }
