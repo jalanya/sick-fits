298ee4ae582990ff2b2578c8595e89a1f07871f9
Jose Alanya <jose.alanya@gmail.com>
2021-05-20 18:59:18 -0500

    FEAT: Shopping Cart Development: Cart - Creating the Data Type and Two Way

--- backend/keystone.ts
+++ backend/keystone.ts
@@ -6,6 +6,7 @@ import {
   statelessSessions,
 } from '@keystone-next/keystone/session';
 import { User } from './schemas/User';
+import { CartItem } from './schemas/CartItem';
 import { Product } from './schemas/Product';
 import { ProductImage } from './schemas/ProductImage';
 import { insertSeedData } from './seed-data';
@@ -58,6 +59,7 @@ export default withAuth(
       User,
       Product,
       ProductImage,
+      CartItem,
     }),
     ui: {
       // Show the UI only for who pass this test
